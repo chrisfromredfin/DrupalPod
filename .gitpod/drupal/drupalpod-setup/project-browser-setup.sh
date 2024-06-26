04521753065b32312f61b8b1fbefcc6f0e9bdb12
Christopher J. Wells <chris@redfinsolutions.com>
2024-06-26 13:24:44 -0400

    some extra nice-to-haves

--- .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
+++ .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 ddev composer remove mglaman/composer-drupal-lenient
 ddev composer require drupal/automatic_updates:^3.1.3
+ddev drush pmu -y admin_toolbar
 ddev drush en -y project_browser package_manager
+ddev drush en -y navigation
 ddev drush cset -y package_manager.settings include_unknown_files_in_project_root true
+ddev drush cset -y project_browser.admin_settings allow_ui_install true
+ddev drush cr
