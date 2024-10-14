31ba507e7aa337cbd42a2dd66a542f8fbedf608d
Christopher J. Wells <chris@redfinsolutions.com>
2024-10-14 15:11:18 -0400

    turn off core-dev so that it's a no-questions-asked install

--- .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
+++ .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ddev composer remove drupal/core-dev
 ddev composer remove mglaman/composer-drupal-lenient
 ddev composer require drupal/automatic_updates:^3.1.3
 ddev drush pmu -y admin_toolbar
@@ -6,4 +7,5 @@ ddev drush en -y project_browser package_manager
 ddev drush en -y navigation
 ddev drush cset -y package_manager.settings include_unknown_files_in_project_root true
 ddev drush cset -y project_browser.admin_settings allow_ui_install true
+ddev drush cset -y --input-format=yaml package_manager.settings additional_trusted_composer_plugins \[tbachert/spi\]
 ddev drush cr
